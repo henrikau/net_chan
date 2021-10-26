bc8b55ca0431a2c5720d53c85a384537fef3066b
Henrik Austad <henrik@austad.us>
2021-10-26 15:03:48 +0200

    code: add script to watch code and trigger build and test (inotifywait)

---
+++ code/scripts/watch_builder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# require: inotify-tools
+
+# find directorires with files
+pushd "$(dirname $(dirname $(realpath -s $0)))"
+
+test -z ${NUM_CPUS} && NUM_CPUS=$(($(nproc)*2))
+
+dirs=$(for f in $(find . -name "*.[c|h]" -o -name "CMakeLists.txt"); do dirname ${f}; done|grep -v build|sort|uniq)
+if [[ -z ${dirs} ]]; then
+    echo "Could not find any source-files, try to move to the root of the project!"
+    exit 1
+fi
+echo ${dirs}
+
+# This is a fairly naive approach. Once an event is detected,
+# inotifywait will return and a new process is started. This means that
+# if more events are happening while build.sh is running, we will miss
+# them.
+while inotifywait -e modify -e create --exclude "\#" ${dirs} ; do
+    test -d build/ || { mkdir build; meson build; }
+    ninja -C build/
+    pushd build > /dev/null
+    time meson test -v
+    popd > /dev/null
+
+    dirs=$(for f in $(find . -name "*.[c|h]" -o -name "CMakeLists.txt"); do dirname ${f}; done|grep -v build|sort|uniq)
+done
+
+popd > /dev/null
