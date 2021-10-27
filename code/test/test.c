20e68055d6decb13626a8a9a4f80670ceb99622c
Henrik Austad <henrik@austad.us>
2021-10-27 11:42:17 +0200

    code/test: test feed_pdu

--- code/test/test.c
+++ code/test/test.c
@@ -15,18 +15,22 @@ struct timedc_avtp *pdu17;
 char data42[DATA42SZ] = {0};
 struct timedc_avtp *pdu42;
 
+struct nethandler *nh;
+
 void setUp(void)
 {
 	pdu17 = pdu_create(17, DATA17SZ);
 	pdu42 = pdu_create(42, DATA42SZ);
 	memset(data42, 0x42, DATA42SZ);
 	memset(data17, 0x17, DATA17SZ);
+	nh = nh_init((unsigned char *)"lo", 16, (unsigned char *)"01:00:e5:01:02:42");
 }
 
 void tearDown(void)
 {
 	pdu_destroy(&pdu17);
 	pdu_destroy(&pdu42);
+	nh_destroy(&nh);
 }
 
 static void test_pdu_create(void)
@@ -82,15 +86,15 @@ int nh_callback(void *data, struct timedc_avtp *pdu)
 
 static void test_nh_hashmap(void)
 {
-	printf("%s(): start\n", __func__);
-	struct nethandler *nh;
 	unsigned char *cb_priv_data = malloc(32);
 	if (!cb_priv_data)
 		return;
-	printf("testing hashmap\n");
-	nh = nh_init((unsigned char *)"lo", 16, (unsigned char *)"14:da:e9:2b:0a:c1");
+
 	TEST_ASSERT(nh->hmap_sz == 16);
-	TEST_ASSERT(nh_reg_callback(NULL, 17, cb_priv_data, nh_callback) == -EINVAL);
+	TEST_ASSERT(nh_reg_callback(NULL, 16, cb_priv_data, nh_callback) == -EINVAL);
+	TEST_ASSERT(nh_reg_callback(nh, 16, NULL, NULL) == -EINVAL);
+
+	TEST_ASSERT(nh_reg_callback(nh, 15, NULL, nh_callback) == 0);
 	TEST_ASSERT(nh_reg_callback(nh, 16, cb_priv_data, nh_callback) == 0);
 	TEST_ASSERT(nh->hmap[0].priv_data == cb_priv_data);
 	TEST_ASSERT(nh->hmap[0].cb == nh_callback);
@@ -106,11 +110,44 @@ static void test_nh_hashmap(void)
 	free(cb_priv_data);
 }
 
+static void test_nh_feed_pdu(void)
+{
+	unsigned char *cb_priv_data = malloc(32);
+	if (!cb_priv_data)
+		return;
+	memset(cb_priv_data, 0xa0, 32);
+
+	TEST_ASSERT(nh_feed_pdu(NULL, NULL) == -EINVAL);
+	TEST_ASSERT(nh_feed_pdu(nh, pdu42) == -EBADFD);
+
+	TEST_ASSERT(nh_reg_callback(nh, 16, cb_priv_data, nh_callback) == 0);
+	TEST_ASSERT(nh_feed_pdu(nh, pdu42) == -EBADFD);
+
+	TEST_ASSERT(nh_reg_callback(nh, 42, cb_priv_data, nh_callback) == 0);
+	TEST_ASSERT(cb_pdu == NULL);
+	TEST_ASSERT(cb_data == NULL);
+	TEST_ASSERT(nh_feed_pdu(nh, pdu42) == 0);
+	TEST_ASSERT(cb_pdu == pdu42);
+	TEST_ASSERT(cb_data == cb_priv_data);
+	TEST_ASSERT(((struct timedc_avtp *)cb_pdu)->pdu.stream_id == 42);
+
+	TEST_ASSERT(((unsigned char *)cb_data)[0] == 0xa0);
+	TEST_ASSERT(((unsigned char *)cb_data)[31] == 0xa0);
+
+	/* verify that calling feed_pdu will call cb with correct data */
+	TEST_ASSERT(nh_reg_callback(nh, 17, cb_priv_data, nh_callback) == 0);
+	TEST_ASSERT(nh_feed_pdu(nh, pdu17) == 0);
+	TEST_ASSERT(((struct timedc_avtp *)cb_pdu)->pdu.stream_id == 17);
+	TEST_ASSERT(nh_feed_pdu(nh, pdu42) == 0);
+	TEST_ASSERT(((struct timedc_avtp *)cb_pdu)->pdu.stream_id == 42);
+}
+
 int main(int argc, char *argv[])
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_pdu_create);
 	RUN_TEST(test_pdu_update);
 	RUN_TEST(test_nh_hashmap);
+	RUN_TEST(test_nh_feed_pdu);
 	return UNITY_END();
 }
