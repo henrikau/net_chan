4dcad816c4381f1bebe6975fd015843dc1889d5e
Henrik Austad <henrik@austad.us>
2021-11-19 13:33:53 +0100

    code: update example net_fifo
    - drop nf_hmap size
    - use designated initializers to make code easier to read

--- code/src/net_fifo.h
+++ code/src/net_fifo.h
@@ -1,31 +1,41 @@
 #pragma once
 #include <timedc_avtp.h>
 
-/* nethandler stuff */
-int nf_hmap_size = 42;
-
-/* channels */
-
 struct net_fifo net_fifo_chans[] = {
 	{
-		{0x01, 0x00, 0x5E, 0x00, 0x00, 0x00},
-		42,
-		8,
-		50,
-		"test42"
+		.dst       = DEFAULT_MCAST,
+		.stream_id = 42,
+		.size      =  8,
+		.freq      = 50,
+		.name      = "mcast42"
 	},
 	{
-		{0x01, 0x00, 0x5E, 0x00, 0x00, 0x01},
-		43,
-		8,
-		10,
-		"test43"
+		.dst       = {0x01, 0x00, 0x5E, 0x00, 0x00, 0x01},
+		.stream_id = 43,
+		.size      = 8,
+		.freq      = 10,
+		.name      = "mcast43"
 	},
 	{
-		{0x01, 0x00, 0x5E, 0x00, 0x00, 0x03},
-		44,
-		8,
-		10,
-		"my-fifo"
+		.dst       = {0x01, 0x00, 0x5E, 0x00, 0x00, 0x03},
+		.stream_id = 44,
+		.size      = 8,
+		.freq      = 10,
+		.name      = "my-fifo"
+	},
+	{
+		.dst       = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		.stream_id = 1337,
+		.size      = 8,
+		.freq      = 10,
+		.name      = "bcast",
+	},
+	{
+
+		.dst       = {0x24, 0x5e,0xbe, 0x54, 0x2e, 0x23},
+		.stream_id = 31337,
+		.size      = 8,
+		.freq      = 100, /* every 10ms */
+		.name	   = "uni100",
 	},
 };
