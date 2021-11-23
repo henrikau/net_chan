92831effc06bb56111c3ff592353f892a0dfb983
Henrik Austad <henrik@austad.us>
2021-11-24 00:29:58 +0100

    code: use designated initializers to improve readability

--- code/test/test_net_fifo.h
+++ code/test/test_net_fifo.h
@@ -3,18 +3,18 @@
 
 struct net_fifo net_fifo_chans[] = {
 	{
-		{0x01, 0x00, 0x5E, 0x00, 0x00, 0x00},
-		42,
-		8,
-		50,
-		"test1"
+		.dst       = {0x01, 0x00, 0x5E, 0x00, 0x00, 0x00},
+		.stream_id = 42,
+		.size      = 8,
+		.freq      = 50,
+		.name      = "test1"
 	},
 	{
-		{0x01, 0x00, 0x5E, 0xde, 0xad, 0x42},
-		43,
-		8,
-		10,
-		"test2"
+		.dst       = {0x01, 0x00, 0x5E, 0xde, 0xad, 0x42},
+		.stream_id = 43,
+		.size      = 8,
+		.freq      = 10,
+		.name      = "test2"
 	},
 };
 int nfc_sz = ARRAY_SIZE(net_fifo_chans);
