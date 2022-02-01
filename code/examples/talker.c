268f828ba5efa48172f0ab9b60dc7f30d450dfe4
Henrik Austad <henrik@austad.us>
2022-02-01 01:09:02 +0100

    code: add sighandler to talker example
    We want to run a proper teardown, so catch sigabrt and do cleanup

--- code/examples/talker.c
+++ code/examples/talker.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
@@ -5,14 +6,24 @@
 #include <timedc_args.h>
 
 #include "net_fifo.h"
+static int running = 0;
+
+void sighandler(int signum)
+{
+	printf("%s(): Got signal (%d), closing\n", __func__, signum);
+	fflush(stdout);
+	running = 0;
+}
 
 int main(int argc, char *argv[])
 {
 	GET_ARGS();
 
 	NETFIFO_TX(mcast42);
+	running = 1;
 
-	for (uint64_t i = 0; i < 10; i++) {
+	signal(SIGINT, sighandler);
+	for (uint64_t i = 0; i < 100000 && running; i++) {
 		WRITE(mcast42, &i);
 		printf("%lu: written\n", i);
 		usleep(100000);
