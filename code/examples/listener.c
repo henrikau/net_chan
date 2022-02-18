dd3ad3cf389e1dbef521a5cc6952a70f4214f7db
Henrik Austad <henrik@austad.us>
2022-02-18 14:23:49 +0100

    code: use (READ|WRITE)_WAIT in example

--- code/examples/listener.c
+++ code/examples/listener.c
@@ -11,8 +11,9 @@ int main(int argc, char *argv[])
 	NETFIFO_RX(mcast42);
 	while (1) {
 		uint64_t d = 0;
-		READ(mcast42, &d);
-		printf("Counter received! -> %lu\n", d);
+		READ_WAIT(mcast42, &d);
+		if (!(d%10))
+			printf("Counter received! -> %lu\n", d);
 	}
 
 	CLEANUP();
--- code/examples/talker.c
+++ code/examples/talker.c
@@ -23,10 +23,16 @@ int main(int argc, char *argv[])
 	running = 1;
 
 	signal(SIGINT, sighandler);
-	for (uint64_t i = 0; i < 100000 && running; i++) {
-		WRITE(mcast42, &i);
-		printf("%lu: written\n", i);
-		usleep(100000);
+	for (uint64_t i = 0; i < 500 && running; i++) {
+		WRITE_WAIT(mcast42, &i);
+		if (!(i%10))
+			printf("%lu: written\n", i);
+
+		/* Take freq and subtract expected wait from class to
+		 * get roughly right delay
+		 */
+		int udel = 1000000 / net_fifo_chans[0].freq - (net_fifo_chans[0].class == CLASS_A ? 2*US_IN_MS : 50*US_IN_MS);
+		usleep(udel);
 	}
 
 	CLEANUP();
