976930d0d5af78d95b00eaa2d741fe8cbdcfa68b
Henrik Austad <henrik@austad.us>
2023-02-02 10:04:42 +0100

    [WIP] Update NIC BW calculations

--- scripts/nic_bw.py
+++ scripts/nic_bw.py
@@ -1,6 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import argparse
+import math
 import sys
+
+VERBOSE = False
+ETHER = 18 + 4  # Ethernet header + CRC + VLAN
+L1 = 7 + 1 + 12 # Preamble, start, IPG
+CSHDR = 24
+MIN_PAYLOAD = 46
+
+def do_print(line):
+    if VERBOSE:
+        print(line)
 
 """
 
@@ -11,6 +22,23 @@ Based on 802.1Q Annex L, class A should be OK, B requires more work to
 verify.
 
 """
+def print_channels(channels):
+    """
+    Pretty print all channels found
+    """
+    if not channels:
+        return
+    # Split by channels
+    cha = [channels[x] for x in channels.keys() if channels[x]['sc'] == 'CLASS_A']
+    chb = [channels[x] for x in channels.keys() if channels[x]['sc'] == 'CLASS_B']
+    print(" Class | StreamID |     Name     |  Hz  | sz")
+    print("-------+----------+--------------+------+---------")
+    for ch in cha:
+        print(" {:^5s} | {:^8s} | {:>12s} | {:>4s} | {}".format("A", ch['stream_id'], ch['name'], ch['freq'], ch['size']))
+    for ch in chb:
+        print(" {:^5s} | {:^8s} | {:>12s} | {:>4s} | {}".format("B", ch['stream_id'], ch['name'], ch['freq'], ch['size']))
+
+
 
 def valid(block):
     if not block:
@@ -19,19 +47,35 @@ def valid(block):
         return False
     return True
 
-
+  
 def calc_bw_req(channel):
     """
     Find BW requirements for a single channel"
     """
+    # Minimum payloadsize for ethernet is 46 bytes, this includes any protocol header 
+    # (like the AVTP header)
+    # For netchan, this is Common Transport Header, 24 bytes.
+    # AVB operates with worst-case sample (i.e. for 125us interval for 48kHz sampling, one should reserve 
+    # for 7 sample-sets pr frame), we ignore this here.
+    payload = int(channel['size']) + CSHDR
+    if payload < MIN_PAYLOAD:
+        payload = MIN_PAYLOAD
+    size = payload + ETHER + L1
+    dps = size * int(channel['freq']) * 8
+    idle_slope = dps / 1e6
+    
+    do_print("calc_bw_req()")
     for k in channel.keys():
-        print("{:12s}: {}".format(k, channel[k]))
-    dps = int(channel['size']) * 8 * int(channel['freq'])
-    print("{}\n\tbps:       {}".format(channel['name'], dps))
-    print(    "\tidleSlope: {}".format(dps / 1e6))
-
-    print("")
-    return dps / 1000
+        do_print("{:12s}: {}".format(k, channel[k]))
+    do_print("\t{:12s}: {}".format("Header overhead", ETHER+L1))
+    do_print("\t{:12s}: {}".format("Payload data", channel['size']))
+    do_print("\t{:12s}: {}".format("Full payload size", payload))
+    do_print("\t{:12s}: {}".format("Frame-size", size))
+    do_print("\t{:12s}: {:.3f} %".format("SnR", 100.0 * float(channel['size']) / size))
+    do_print("\t{:12s}: {}".format("dps", dps))
+    do_print("\t{:12s}: {}".format("idleSlope", dps / 1e6))
+    do_print("")
+    return int(math.ceil(dps / 1000))
 
 def line_clean(line):
     # strip away comments
@@ -44,7 +88,7 @@ def line_clean(line):
     return line.strip()
 
 
-def parse_file(manifest, verbose=False):
+def parse_file(manifest):
     channels = {}
     block = {}
     found_net_fifo = False
@@ -56,16 +100,14 @@ def parse_file(manifest, verbose=False):
             if not found_net_fifo:
                 if "struct net_fifo" in line:
                     found_net_fifo = True
-                    if verbose:
-                        print("Found beginning of struct")
+                    do_print("Found beginning of struct")
                 continue
             else:
                 # Star/end of a block, update ctr accordingly to find
                 # when to dump a complete block into channels
                 if '{' in line:
                     lvlctr += 1
-                    if verbose:
-                        print("new block -> {}".format(lvlctr))
+                    do_print("new block -> {}".format(lvlctr))
                 if '}' in line:
                     lvlctr -= 1
                     if block:
@@ -78,13 +120,11 @@ def parse_file(manifest, verbose=False):
                         else:
                             print("Invalid channelblock detected!")
                             print("This won't be added, please have a look at {}".format(manifest))
-                            if verbose:
-                                print("incomplete block: ")
-                                print(block)
+                            print("incomplete block: ")
+                            print(block)
 
                     block = {}
-                    if verbose:
-                        print("end of block -> {}".format(lvlctr))
+                    do_print("end of block -> {}".format(lvlctr))
 
                 # In a valid block, grab key and token and populate block
                 if lvlctr > 0:
@@ -95,15 +135,12 @@ def parse_file(manifest, verbose=False):
 
 
             if lvlctr < 0:
-                if verbose:
-                    print("End of struct")
+                do_print("End of struct")
                 found_net_fifo = False
                 break
     return channels
 
-def main(manifest, txstreams, linkspeed, verbose):
-    channels = parse_file(manifest, verbose)
-
+def main(channels, txstreams, linkspeed, verbose):
     txs = [x.strip() for x in txstreams.split(',')]
     kbps_a = 0
     kbps_b = 0
@@ -111,7 +148,7 @@ def main(manifest, txstreams, linkspeed, verbose):
         if t not in channels:
             print("Did not find {} in channels!".format(t))
         else:
-            if channels[t]['class'] == 'CLASS_A':
+            if channels[t]['sc'] == 'CLASS_A':
                 kbps_a += calc_bw_req(channels[t])
             else:
                 kbps_b += calc_bw_req(channels[t])
@@ -149,7 +186,7 @@ def main(manifest, txstreams, linkspeed, verbose):
     loCredit_a = int(sendSlope_a * interferenceTime_a)
 
     # Largets interference from this queue (required for B)
-    maxBurstSize_a = int(linkspeed) * (hiCredit_a - loCredit_a)/(-sendSlope_a)
+    maxBurstSize_a = int(linkspeed * (hiCredit_a - loCredit_a)/(-sendSlope_a))
 
 
     # Now that we have all of Class A's values, we can find B. See
@@ -162,7 +199,7 @@ def main(manifest, txstreams, linkspeed, verbose):
     maxBurstSize_b = 0
 
     print("{}".format("="*80))
-    print("found {} channels in manifest".format(len(channels)))
+    print("Found {} channels in manifest".format(len(channels)))
     print("Linkspeed: {:.2f} Mbps".format(int(linkspeed) / 1000.0))
     print("CBS Class A:")
     print("\tbandwidth: {:8d} kbps".format(kbps_a))
@@ -189,13 +226,14 @@ def main(manifest, txstreams, linkspeed, verbose):
 
     print("{}".format("="*80))
     print("Commands to run on host (use correct nic and parent!")
-    print("\tsudo tc qdisc replace dev enp2s0 parent root mqprio num_tc 4 \\")
+    print("\texport CBSNIC=enp2s0")
+    print("\tsudo tc qdisc replace dev ${CBSNIC} parent root mqprio num_tc 4 \\")
     print("\t\tmap 3 3 1 0 2 2 2 2 2 2 2 2 2 2 2 2 queues 1@0 1@1 1@2 1@3 hw 0")
     print("\tsudo tc qdisc (add|replace) dev {} parent {} cbs idleslope {} sendslope -{} \\".\
-          format("enp2s0", "8003:1", idleSlope_a, sendSlope_a))
+          format("${CBSNIC}", "8003:1", idleSlope_a, sendSlope_a))
     print("\t\thicredit {} locredit -{} offload 1".format(hiCredit_a, loCredit_a))
     print("\tsudo tc qdisc (add|replace) dev {} parent {} cbs idleslope {} sendslope -{} \\".\
-          format("enp2s0", "8003:2", idleSlope_b, sendSlope_b))
+          format("${CBSNIC}", "8003:2", idleSlope_b, sendSlope_b))
     print("\t\thicredit {} locredit -{} offload 1".format(hiCredit_b, loCredit_b))
 
     print("to inspect:")
@@ -205,14 +243,21 @@ def main(manifest, txstreams, linkspeed, verbose):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Calculate idleSlope, sendSlope, hiCredit and loCredit based on argumentlist and manifest.')
-    parser.add_argument('-m', '--manifest', dest='manifest')
-    parser.add_argument('-v', '--verbose' , action="store_true", default=False)
-    parser.add_argument('-t', '--txstreams', dest='txstreams', help="name of outgoing streams on this system")
-    parser.add_argument('-L', '--link-speed', dest='linkspeed', help="Speed of link we're configuring in kbit/s.", default=1000000)
+    parser.add_argument('-m', '--manifest',   dest='manifest')
+    parser.add_argument('-v', '--verbose' ,   action="store_true", default=False)
+    parser.add_argument('-t', '--txstreams',  dest='txstreams',   help="name of outgoing streams on this system")
+    parser.add_argument('-l', '--list',       action="store_true", default=False, help="Provide a (short)list of all streams declared in the manifest")
+    parser.add_argument('-L', '--link-speed', dest='linkspeed',   help="Speed of link we're configuring in kbit/s.", default=1000000)
     args = parser.parse_args()
+    VERBOSE = args.verbose
+
     if not args.manifest:
         print("Need manifest-file to parse")
         parser.print_help(sys.stderr)
         sys.exit(1)
-
-    main(args.manifest, args.txstreams, args.linkspeed, args.verbose)
+    
+    channels = parse_file(args.manifest)
+    if args.list:
+        print_channels(channels)
+    else:
+        main(channels, args.txstreams, args.linkspeed, args.verbose)
