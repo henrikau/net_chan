06d7f34b3754f54a8029a5df682625f73d32e13b
Henrik Austad <henrik@austad.us>
2023-06-09 14:47:49 +0200

    Do not attempt to fix permissions for removed tests.

--- scripts/fix_perms.sh
+++ scripts/fix_perms.sh
@@ -3,8 +3,6 @@ pushd "$(dirname $(dirname $(realpath -s $0)))/build"
 sudo setcap cap_net_raw,cap_net_admin=eip testnetfifo
 sudo setcap cap_net_raw,cap_net_admin=eip testpdu
 sudo setcap cap_net_raw,cap_net_admin=eip testnh
-sudo setcap cap_net_raw,cap_net_admin=eip testnfmacro
-sudo setcap cap_net_raw,cap_net_admin,cap_sys_nice=eip testmrp
 sudo setcap cap_net_raw,cap_net_admin=eip talker
 sudo setcap cap_net_raw,cap_net_admin=eip listener
 popd
