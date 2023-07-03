b2aa0c867fe462088e37ad70612a639caf7a59b6
Henrik Austad <henrik@austad.us>
2023-07-03 16:53:48 +0200

    Add talker/listener pair for C++ wrapper
    the Meson build script does not like mixing C and C++, so these to
    examples are not included in the main meson build.

---
+++ examples/listener.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 SINTEF AS
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+ */
+
+// This file is not included in the meson build script (meson does not
+// like mixing C and C++ in the same project).
+//
+// To compile:
+// 1. build netchan
+// 2. g++ -o build/cpp_listener examples/listener.cpp -I include -L build/ -lnetchan
+
+#include <iostream>
+#include <netchan.hpp>
+#include "manifest.h"
+
+int main()
+{
+    netchan::NetHandler nh("enp6s0", false);
+    netchan::NetChanRx rx(nh, &net_fifo_chans[0]);
+
+    struct sensor s = { 0 };
+    for (int i = 0; i < 1000; i++) {
+        if (rx.read(&s)) {
+            printf("%lu : %lx\n", s.seqnr, s.val);
+            if (s.seqnr == -1)
+                break;
+        }
+    }
+
+    return 0;
+}
---
+++ examples/talker.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 SINTEF AS
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+ */
+
+// This file is not included in the meson build script (meson does not
+// like mixing C and C++ in the same project).
+//
+// To compile:
+// 1. build netchan
+// 2. g++ -o build/cpp_talker examples/talker.cpp -I include -L build/ -lnetchan
+
+#include <iostream>
+#include <netchan.hpp>
+#include <unistd.h>
+#include "manifest.h"
+
+int main()
+{
+    netchan::NetHandler nh("enp6s0", false);
+    netchan::NetChanTx tx(nh, &net_fifo_chans[0]);
+
+    struct sensor s = { .val = 0xdeadbeefa0a0a0a0, .seqnr = 1337 };
+    for (int i = 0; i < 50; i++) {
+        s.seqnr = i;
+        if (!tx.send(&s)) {
+            printf("%s(): sending failed\n", __func__);
+            break;
+        }
+        usleep(100000);
+    }
+    s.seqnr = -1;
+    tx.send(&s);
+
+    return 0;
+}
