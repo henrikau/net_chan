98959a1851b99938891f77c94ac50faa5a471b76
Henrik Austad <henrik@austad.us>
2023-12-01 14:22:45 +0100

    scripts: adjust for leap seconds and improve text output

--- scripts/analyze_tr.py
+++ scripts/analyze_tr.py
@@ -25,6 +25,11 @@ def filter_sids(dft, dfl, sids):
             df_l = df_l.drop(['rx_ns', 'recv_ptp_ns'], axis=1)
             df_t = df_t.drop(['cap_ptp_ns', 'send_ptp_ns', 'tx_ns'], axis=1)
             df = pd.merge(df_t, df_l, on=['stream_id', 'avtp_ns', 'seqnr', 'sz'], suffixes=['_l', '_t'])
+        # Adjust for LEAP second adjustment (CLOCK_REALTIME is 37 seconds ahead)
+        df['send_ptp_ns'] += int(37*1e9)
+        df['tx_ns'] += int(37*1e9)
+        df['rx_ns'] += int(37*1e9)
+
         df['cap2tx']  = df['tx_ns'] - df['cap_ptp_ns']
         df['tx2rx']   = df['rx_ns'] - df['tx_ns']
         df['rx2recv'] = df['recv_ptp_ns'] - df['rx_ns']
@@ -40,19 +45,16 @@ def filter_sids(dft, dfl, sids):
         dfs[sid] = df
     return dfs
 
-def print_stats_field(df, title, field):
-    print(title)
-    print(f"Max  : {max(df[field])/1e3:8.3f} us")
-    print(f"Min  : {min(df[field])/1e3:8.3f} us")
-    print(f"Avg  : {statistics.mean(df[field])/1e3:8.3f} us")
-    print(f"stdev: {statistics.stdev(df[field])/1e3:8.3f} us")
-
 
 def print_statistics(sid, df):
-    print(f"Statistics for StreamID={sid}")
-    print_stats_field(df, 'E2E', 'e2e')
-    print_stats_field(df, "Tx Period", 'tx_diff')
-    print_stats_field(df, "Rx Period", 'rx_diff')
+    print(f"{'-'*9}+{'-'*20}+{'-'*20}+{'-'*20}+")
+    print(f"{sid:^9}|{'E2E':^20s}|{'Tx Period':^20s}|{'Rx Period':^20s}|")
+    print(f"{'-'*9}+{'-'*20}+{'-'*20}+{'-'*20}+")
+    print(f"{'Max':8s} | {max(df['e2e'])/1e3:15.3f} us | {max(df['tx_diff'])/1e3:15.3f} us | {max(df['rx_diff'])/1e3:15.3f} us |")
+    print(f"{'Min':8s} | {min(df['e2e'])/1e3:15.3f} us | {min(df['tx_diff'])/1e3:15.3f} us | {min(df['rx_diff'])/1e3:15.3f} us |")
+    print(f"{'Average':8s} | {statistics.mean(df['e2e'])/1e3:15.3f} us | {statistics.mean(df['tx_diff'])/1e3:15.3f} us | {statistics.mean(df['rx_diff'])/1e3:15.3f} us |")
+    print(f"{'StdDev':8s} | {statistics.stdev(df['e2e'])/1e3:15.3f} us | {statistics.stdev(df['tx_diff'])/1e3:15.3f} us | {statistics.stdev(df['rx_diff'])/1e3:15.3f} us |")
+
 
 
 if __name__ == "__main__":
@@ -71,5 +73,4 @@ if __name__ == "__main__":
     print(f"Got {len(dfs)} streams filtered and ready")
 
     for sid in dfs.keys():
-        print(f"{'-'*80}")
         print_statistics(sid, dfs[sid])
