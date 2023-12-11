b27367474fa9e01c8950d3970abec901e65a67f5
Henrik Austad <henrik@austad.us>
2023-12-11 10:19:27 +0100

    scripts: don't run tests if any of the builds (g++) fails

--- scripts/watch_builder.sh
+++ scripts/watch_builder.sh
@@ -20,18 +20,18 @@ echo ${dirs}
 while inotifywait -e modify -e create --exclude "\#" ${dirs} ; do
     test -d build/ || { mkdir build; meson build; }
     ninja -C build/ &&  {
-	echo "Building C++ apps"
-	g++ -o build/cpp_listener examples/listener.cpp build/libnetchan.a build/libmrp.a -lboost_program_options -pthread -I include
-	g++ -o build/cpp_talker examples/talker.cpp build/libnetchan.a build/libmrp.a -lboost_program_options -pthread -I include
-	# only run tests if build was ok
-	./scripts/fix_perms.sh
-	pushd build > /dev/null
-	if [[ ! -z $(which pmccabe) ]]; then
-	    meson test && { pmccabe -v /dev/null | head -n6; pmccabe ../src/* | sort -nk 2 | tail -n20; echo "Files: "; pmccabe -F ../src/* |sort -nk 2; }
-	else
-	    meson test
-	fi
-	popd > /dev/null
+	echo "Building C++ apps" && \
+	g++ -o build/cpp_listener examples/listener.cpp build/libnetchan.a build/libmrp.a -lboost_program_options -pthread -I include && \
+	g++ -o build/cpp_talker examples/talker.cpp build/libnetchan.a build/libmrp.a -lboost_program_options -pthread -I include && {
+	    ./scripts/fix_perms.sh 
+	    pushd build > /dev/null
+	    if [[ ! -z $(which pmccabe) ]]; then
+		meson test && { pmccabe -v /dev/null | head -n6; pmccabe ../src/* | sort -nk 2 | tail -n20; echo "Files: "; pmccabe -F ../src/* |sort -nk 2; }
+	    else
+		meson test
+	    fi
+	    popd > /dev/null
+	}
     }
 
     dirs=$(for f in $(find . -name "*.[c|h]" -o -name "meson.build"); do dirname ${f}; done|grep -v build|sort|uniq)
