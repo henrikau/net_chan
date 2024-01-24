14baaf6b5571637fe0b610f18f447b1bef8e3934
Henrik Austad <henrik@austad.us>
2024-01-24 22:28:57 +0100

    [scripts] add simple installer-script (to be combined with packager later)

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+pushd "$(dirname $(dirname $(realpath -s $0)))" > /dev/null
+
+TMPINSTALL=$(pwd)/build/tmpinstall
+echo "Using tmpinstall=${TMPINSTALL}"
+
+pushd build/ > /dev/null
+test -d ${TMPINSTALL} && rm -rf ${TMPINSTALL}
+mkdir ${TMPINSTALL}
+meson install --destdir ${TMPINSTALL}
+
+pushd ${TMPINSTALL} >/dev/null
+sudo cp -rv usr/local/include* /usr/local/include
+sudo cp -v usr/local/lib/x86_64-linux-gnu/*.so /usr/local/lib/x86_64-linux-gnu/.
+sudo cp -v usr/local/lib/x86_64-linux-gnu/*.a /usr/local/lib/x86_64-linux-gnu/.
+
+popd >/dev/null			# TMPINSTALL
+
+popd >/dev/null			# build
+
+popd >/dev/null			# basepath
