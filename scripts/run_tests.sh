d776b3499344b6153cb1f0dc23bd316af91deb33
Henrik Austad <henrik@austad.us>
2024-02-06 12:35:40 +0100

    [scripts] report full log if tests fail

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,10 +2,16 @@
 set -e
 
 pushd "$(dirname $(dirname $(realpath -s $0)))/build" > /dev/null
-if [[ ! -z $(which pmccabe) ]]; then
-    meson test && { pmccabe -v /dev/null | head -n6; pmccabe ../src/* | sort -nk 2 | tail -n20; echo "Files: "; pmccabe -F ../src/* |sort -nk 2; }
-else
-    meson test
-fi
 
+if ! meson test ; then
+    cat meson-logs/testlog.txt
+else
+    if [[ ! -z $(which pmccabe) ]]; then
+	pmccabe -v /dev/null \
+	    | head -n6; pmccabe ../src/* \
+	    | sort -nk 2 \
+	    | tail -n20; echo "Files: ";
+	pmccabe -F ../src/* |sort -nk 2;
+    fi
+fi
 popd > /dev/null
