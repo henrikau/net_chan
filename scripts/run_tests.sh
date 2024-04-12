a352d167a29ffed7a432e672de488a96601e070a
Henrik Austad <henrik@austad.us>
2024-04-12 13:58:29 +0200

    Re-enable  pmccabe when tests complete successfully

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,11 +9,11 @@ if ! meson test ; then
 else
     if [[ ! -z $(which pmccabe) ]]; then
 	echo "disabling pmccabe for now"
-    # 	pmccabe -v /dev/null \
-    # 	    | head -n6; pmccabe ../src/* \
-    # 	    | sort -nk 2 \
-    # 	    | tail -n20; echo "Files: ";
-    # 	pmccabe -F ../src/* |sort -nk 2;
+	pmccabe -v /dev/null \
+	    | head -n6; pmccabe ../src/* \
+	    | sort -nk 2 \
+	    | tail -n20; echo "Files: ";
+	pmccabe -F ../src/* |sort -nk 2;
     fi
 fi
 popd > /dev/null
